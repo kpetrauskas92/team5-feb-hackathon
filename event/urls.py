43f546f42fd48c8d07e2a273a56a0b7afce0f4c5
Karolis <118168899+kpetrauskas92@users.noreply.github.com>
2024-02-17 19:08:47 +0000

    Updated events, added additional models, updated templates, created forms

--- event/urls.py
+++ event/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import EventPostList, event_details
+from .views import EventPostList, event_details, create_event_post, update_event
 
 urlpatterns = [
     path('event/<slug:slug>/', event_details, name='event'),
     path('eventlist/', EventPostList.as_view(), name='event_list'),
+    path('create/', create_event_post, name='create_event_post'),
+    path('event/update/<slug:slug>/', update_event, name='update_event'),
 ]
-
