c313ab7e979f4ec26e95e6060b0ecf3bcf2f61cf
Karolis <118168899+kpetrauskas92@users.noreply.github.com>
2024-02-17 20:08:36 +0000

    Edit and Delete post

--- event/views.py
+++ event/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
 from django.contrib.auth.decorators import login_required
 from .models import EventPost
+from django.http import HttpResponseForbidden
 from .forms import EventPostForm
+from django.contrib import messages
 
 
 class EventPostList(generic.ListView):
@@ -26,6 +28,7 @@ def create_event_post(request):
     if request.method == 'POST':
         form = EventPostForm(request.POST, request.FILES)
         if form.is_valid():
+            messages.success(request, 'Your event post has been created successfully!')
             event_post = form.save(commit=False)
             event_post.author = request.user
             event_post.save()
@@ -40,16 +43,36 @@ def update_event(request, slug):
     eventpost = get_object_or_404(EventPost, slug=slug)
 
     if request.user != eventpost.author:
+        messages.error(request, 'You are not authorized to edit this event.')
         return redirect('event', slug=slug)
 
     if request.method == 'POST':
         form = EventPostForm(request.POST, request.FILES, instance=eventpost)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Your event post has been updated successfully!')
             return redirect('event', slug=eventpost.slug)
     else:
         form = EventPostForm(instance=eventpost)
 
-    return render(request, 'event/update_event_post.html',
+    return render(request, 'event/edit_event_post.html',
                   {'form': form,
                    'eventpost': eventpost})
+
+
+@login_required
+def delete_event(request, slug):
+    eventpost = get_object_or_404(EventPost, slug=slug)
+
+    if eventpost.author != request.user:
+        messages.error(request, 'You are not authorized to delete this event.')
+        return HttpResponseForbidden()
+
+    if request.method == 'POST':
+        eventpost.delete()
+        messages.success(request, 'The event post has been deleted successfully.')
+        return redirect('event_list')
+
+    # Render the confirmation page for GET requests
+    return render(request,
+                  'event/confirm_delete_event.html', {'eventpost': eventpost})
