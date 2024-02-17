9aeb53c21760ad6581487ea462168ff32cf5a32b
Declan Caulfield <decant09@hotmail.com>
2024-02-17 19:18:08 +0000

    Add 404 page

--- lovesched/urls.py
+++ lovesched/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('', include('event.urls')),
@@ -32,3 +33,5 @@ if settings.DEBUG:
                           document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+
+handler404 = 'lovesched.views.handler404'
