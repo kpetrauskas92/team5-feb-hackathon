15e4bc20b987048b1f17a835206fdada7a7453f6
Karolis <118168899+kpetrauskas92@users.noreply.github.com>
2024-02-17 15:23:05 +0000

    Bug fix on profiles, updated nav

--- profiles/models.py
+++ profiles/models.py
@@ -4,10 +4,11 @@ from cloudinary.models import CloudinaryField
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE),
-    phone_number = models.CharField(max_length=20, null=True, blank=True),
-    first_name = models.CharField(max_length=100, null=True, blank=True),
-    last_name = models.CharField(max_length=100, null=True, blank=True),
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='profile')
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
     image = CloudinaryField('image', default='placeholder')
 
     def __str__(self):
