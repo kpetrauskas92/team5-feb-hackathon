e1b9308d5153de6fa854309cc52479bc9afb4924
Ann Edvinsson <aslin.edvinsson@gmail.com>
2024-02-17 12:08:05 +0000

    Create EventPostList view with url path(, views.EventPostList.as_view(), name=event_list)

--- event/urls.py
+++ event/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import event_details
+from .views import EventPostList
 
 urlpatterns = [
     path('event/<slug:slug>/', event_details, name='event'),
-
+    path('', views.EventPostList.as_view(), name='event_list'),
 ]
