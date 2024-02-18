316d417995c2f64254315c77f36d5a2ffa474e94
Karolis <118168899+kpetrauskas92@users.noreply.github.com>
2024-02-18 23:29:07 +0000

    Profile update2 (#89)
    * Updated profile form
    
    * Updated profile models
    
    * Updated profile views
    
    * Updated styles, calendar, urls, migrations
    
    * Updated profile templates

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from .models import UserProfile
+from .models import UserProfile, UserDate
 
 
 class UserProfileForm(forms.ModelForm):
-    
+
     field_order = ['first_name', 'last_name', 'phone_number']
 
     class Meta:
@@ -28,3 +28,24 @@ class UserProfileForm(forms.ModelForm):
                 placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
                 self.fields[field].label = False
+
+
+class UserDateForm(forms.ModelForm):
+    class Meta:
+        model = UserDate
+        fields = ['date', 'title', 'budget']
+        help_texts = {
+            'budget': 'Enter your budget (€).',
+        }
+        labels = {
+            'budget': 'Cost (€)',
+        }
+        widgets = {
+            'date': forms.DateInput(
+                attrs={'type': 'date',
+                       'class': 'form-input px-4 py-3 rounded-full'}),
+            'title': forms.TextInput(
+                attrs={'class': 'form-input px-4 py-3 rounded-full'}),
+            'budget': forms.NumberInput(
+                attrs={'class': 'form-input px-4 py-3 rounded-full'}),
+        }
