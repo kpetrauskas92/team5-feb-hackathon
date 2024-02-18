f16ff0a6d7ff6d85c905c969cf4d293c427ef56d
Karolis <118168899+kpetrauskas92@users.noreply.github.com>
2024-02-18 23:19:18 +0000

    Updated profile views

--- profiles/views.py
+++ profiles/views.py
@@ -1,22 +1,48 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum
 from event.models import EventPost
 from event.forms import EventPostForm
-from .forms import UserProfileForm
-from .models import UserProfile
+from .forms import UserProfileForm, UserDateForm
+from .models import UserProfile, UserDate
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 
 @login_required
 def profile(request):
     # Query for the user's own posts
-    user_posts = EventPost.objects.filter(author=request.user, status=1).order_by('-created_on')  # Assuming status=1 means published
+    user_posts = EventPost.objects.filter(
+        author=request.user,
+        status=1).order_by('-created_on')
 
-    # Query for suggested posts (example: posts not authored by the user and are published)
-    suggested_posts = EventPost.objects.exclude(author=request.user).filter(status=1).order_by('-created_on')
+    suggested_posts = EventPost.objects.exclude(
+        author=request.user).filter(status=1).order_by('-created_on')
+
+    # Query for the user's dates
+    user_dates = UserDate.objects.filter(user=request.user).order_by('date')
+
+    # Preparing user_dates for FullCalendar
+    user_dates_events = [
+        {
+            'title': user_date.title or "No Title",
+            'start': user_date.date.strftime("%Y-%m-%d"),
+            'allDay': True
+        }
+        for user_date in user_dates
+    ]
+
+    user_dates_json = json.dumps(user_dates_events, cls=DjangoJSONEncoder)
+
+    # Calculate total budget
+    total_budget = user_dates.aggregate(Sum('budget'))['budget__sum'] or 0
 
     return render(request, 'profiles/profile.html', {
         'user_posts': user_posts,
-        'suggested_posts': suggested_posts
+        'suggested_posts': suggested_posts,
+        'user_dates': user_dates,
+        'user_dates_json': user_dates_json,
+        'total_budget': total_budget,
     })
 
 
@@ -37,13 +63,17 @@ def create_event_post_from_profile(request):
 
 @login_required
 def update_profile(request):
-    """ Display the profile from. """
+    """
+    Display and process the profile form.
+    """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=profile)
+        form = UserProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
             form.save()
+
+            return redirect('profile')
     else:
         form = UserProfileForm(instance=profile)
 
@@ -53,3 +83,40 @@ def update_profile(request):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def add_user_date(request):
+    initial_data = {}
+    post_id = request.GET.get('post_id')
+
+    if post_id:
+        post = get_object_or_404(EventPost, pk=post_id)
+        initial_data['title'] = post.event_name
+
+    if request.method == 'POST':
+        form = UserDateForm(request.POST)
+        if form.is_valid():
+            user_date = form.save(commit=False)
+            user_date.user = request.user
+            if post_id:
+                user_date.post = post
+            user_date.save()
+            return redirect('profile')
+    else:
+        form = UserDateForm(initial=initial_data)
+
+    return render(request, 'profiles/add_to_calendar.html', {'form': form})
+
+
+@login_required
+def delete_user_date(request, date_id):
+    user_date = get_object_or_404(UserDate, id=date_id, user=request.user)
+
+    if request.method == 'POST':
+        user_date.delete()
+        return redirect('profile')
+
+    return render(request, 'profiles/confirm_delete_user_date.html', {
+        'user_date': user_date
+    })
