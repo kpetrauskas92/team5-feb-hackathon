2e3bc5ff77e6ac8ec49383d3f0ecc8e9a88d3390
Ann Edvinsson <aslin.edvinsson@gmail.com>
2024-02-19 11:59:42 +0000

    Add docstrings and comments to landing app

--- landing/forms.py
+++ landing/forms.py
@@ -7,6 +7,9 @@ class ContactForm(forms.ModelForm):
     Form class for creating a contact form with 'name', 'email', and
     'message' fields.
     """
+    # Meta class to define metadata options for the form
     class Meta:
+        # Specifies the model to be used for the form
         model = ContactUs
+        # Specifies the fields from the model to include in the form
         fields = ('name', 'email', 'message',)
