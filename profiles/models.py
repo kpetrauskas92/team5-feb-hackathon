2da4ba037a81ebeb992166569aab24276497a112
Karolis <118168899+kpetrauskas92@users.noreply.github.com>
2024-02-19 13:15:22 +0000

    deleted profile placeholder cors issue

--- profiles/models.py
+++ profiles/models.py
@@ -22,7 +22,7 @@ class UserProfile(models.Model):
                            null=True, blank=True)
     first_name = models.CharField(max_length=100, null=True, blank=True)
     last_name = models.CharField(max_length=100, null=True, blank=True)
-    image = CloudinaryField('image', default='placeholder')
+    image = CloudinaryField('image')
 
     def __str__(self):
         return self.user.username
