ee7621f9d639bf8a2992c90d646a9cca94e872d3
Ann Edvinsson <aslin.edvinsson@gmail.com>
2024-02-19 10:56:35 +0000

    Add docstrings and comments to .py files in Event app

--- event/forms.py
+++ event/forms.py
@@ -9,6 +9,17 @@ STATUS_CHOICES = (
 
 
 class EventPostForm(forms.ModelForm):
+    """
+    A Django form for creating and updating EventPost objects.
+
+    This form includes all necessary fields for an EventPost and customizes the
+    'status' field to use a dropdown select widget, ensuring the user must make
+    a choice. The 'description' field is customized with a Textarea widget to
+    improve user experience by providing a larger area for text input.
+
+    """
+    # Custom field definition for 'status' to ensure it uses a select widget
+    # and is required.
     status = forms.ChoiceField(choices=STATUS_CHOICES,
                                widget=forms.Select(), required=True)
 
@@ -16,6 +27,8 @@ class EventPostForm(forms.ModelForm):
         model = EventPost
         fields = ['event_name', 'status', 'description', 'category',
                   'location', 'image']
+         # Custom widget settings for fields to enhance UI. For example,
+         # increasing the size of the 'description' textarea.
         widgets = {
             'description': forms.Textarea(attrs={'rows': 4}),
         }
